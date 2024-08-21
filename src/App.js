02acf3bcc05dfee4c499cdd80876b1870e766f3f
André Oliveira <andrev-eng@ufmg.br>
2024-08-20 22:28:33 -0300

    Teste de deploy

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nova site da ISS Comercio
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.isscomercio.com.br/
           target="_blank"
-          rel="noopener noreferrer"
+        rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        Antigo site
+      </a>
+    </header>
+    </div >
   );
 }
 
